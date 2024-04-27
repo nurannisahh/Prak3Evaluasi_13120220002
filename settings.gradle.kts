c6d49a5fa07e7d13329e73a184bcf048c8e2f886
Naull <152953821+nurannisahh@users.noreply.github.com>
2024-04-27 19:19:33 +0800

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Prak3Evaluasi_13120220002"
+include(":app")
+ 
